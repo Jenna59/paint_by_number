acd269baf870a880aadc5b0dbaaa40d7fe33aad2
Jenn Cox <jenncox@Jenns-MacBook-Air.local>
2023-06-10 07:58:51 -0400

    added reset button

--- script.js
+++ script.js
@@ -1,47 +1,81 @@
-const red = document.querySelector(".one"); 
-const blue = document.querySelector(".four");
+"use strict";
 
-const iterateOverGrey = 
-	document.querySelectorAll(".three").forEach(square => {
-		square.addEventListener("click", e => {
-			square.style.backgroundColor = 'rgb(202, 219, 229)'
+const colors = {
+	red : "rgb(217, 56, 41)", 
+	yellow: "rgb(235, 211, 83)",
+	gray: "rgb(202, 219, 229)",
+	blue: "rgb(40, 63, 113)",
+	black: "rgb(0, 0, 0)",
+	white: "rgb(227, 230, 235)",
+  beige: "rgb(250,235,215)"
+};
+
+const elemsToPaint = document.querySelectorAll("div.paint");
+
+function paint() {
+
+		elemsToPaint.forEach(elemToPaint => {
+			elemToPaint.addEventListener("click", e => { 
+        const className = e.target.className;
+				switch(className) {
+					case "paint red":
+						e.target.style.backgroundColor = colors.red;
+						e.target.style.color = colors.red;
+					break
+					case "paint yellow":
+						e.target.style.backgroundColor = colors.yellow
+						e.target.style.color = colors.yellow;
+					break
+					case "paint gray":
+						e.target.style.backgroundColor = colors.gray
+						e.target.style.color = colors.gray;
+					break
+					case "paint blue":
+						e.target.style.backgroundColor = colors.blue
+						e.target.style.color = colors.blue;
+					break
+					case "paint black":
+						e.target.style.backgroundColor = colors.black
+						e.target.style.color = colors.black;
+					break
+					case "paint white":
+						e.target.style.backgroundColor = colors.white
+						e.target.style.color = colors.white;
+					break;	
+				}
+			})
 		})
-	});
+}
 
-const iterateOverWhite = 
-	document.querySelectorAll(".six").forEach(square => {
-		square.addEventListener("click", e => {
-			square.style.backgroundColor = 'rgb(227, 230, 235)';
-		})
-	});
+paint();
 
-const iterateOverYellow = 
-	document.querySelectorAll(".two").forEach(square => {
-		square.addEventListener("click", e => {
-			square.style.backgroundColor = 'rgb(235, 211, 83)';
-		})
-	});
+function reset() {
+  const resetBtn = document.querySelector("button")
+  resetBtn.addEventListener("click", () => { 
+    elemsToPaint.forEach(elemToPaint => {
+      elemToPaint.style.backgroundColor = colors.beige;
+      elemToPaint.style.color = colors.black;
+      console.log(elemToPaint)
+    })
+  })
+}
 
-const iterateOverBlack = 
-document.querySelectorAll(".five").forEach(square => {
-		square.addEventListener("click", e => {
-			square.style.backgroundColor = 'rgb(0, 0, 0)';
+reset(); 
 
-		})
-	});
 
-function paintRed() {
-	const red = document.querySelector(".one"); 
-	if (red) {
-    red.style.backgroundColor = 'rgb(217, 56, 41)';
-  }; 
-}; 
 
-function paintBlue() {
-	if (blue) {
-    blue.style.backgroundColor = 'rgb(40, 63, 113)';
-  }; 
-}; 
 
-red.addEventListener('click', paintRed);
-blue.addEventListener('click', paintBlue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
